191a0b846d8e4619c616a720d9d4a6ea0e596d8d
sonalkumar2004 <sonalkumar200429@gmail.com>
2026-03-28 23:02:12 +0530

    first

---
+++ backend/routes/auth.routes.js
@@ -0,0 +1,15 @@
+import express from "express"
+import{signUp, logOut,login} from"../controllers/auth.controllers.js"
+
+
+const authRouter = express.Router()
+authRouter.post("/signup",signUp)
+
+authRouter.post("/login",login)
+authRouter.get("/logout",logOut)
+
+
+
+
+
+export default authRouter
---
+++ frontend/src/redux/store.js
@@ -0,0 +1,12 @@
+import {configureStore} from "@reduxjs/toolkit"
+import userSlice from "./userSlice"
+import messageSlice from "./messageSlice"
+export const store=configureStore({
+    reducer:{
+        user:userSlice,
+        message:messageSlice
+    }
+})
+
+
+ export default store
