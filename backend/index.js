a36ef3aca82cc15a6e223aa9afc6380f157e70a4
sonal kumar shrivastawa <sonalkumar200429@gmail.com>
2026-05-06 00:28:37 +0530

    Update CORS origin for local development

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ const port=process.env.PORT || 5000
 
 
 app.use(cors({
-    origin:"https://real-time-chat-frontend-fr6u.onrender.com",
+    origin:"https://localhost:5173",
     credentials:true
 }))
 app.use(express.json())
