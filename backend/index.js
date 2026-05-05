79b3da693a11c5e58f93a8740ed375b1eb58202c
sonal kumar shrivastawa <sonalkumar200429@gmail.com>
2026-05-06 00:31:09 +0530

    Change CORS origin from HTTPS to HTTP

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ const port=process.env.PORT || 5000
 
 
 app.use(cors({
-    origin:"https://localhost:5173",
+    origin:"http://localhost:5173",
     credentials:true
 }))
 app.use(express.json())
