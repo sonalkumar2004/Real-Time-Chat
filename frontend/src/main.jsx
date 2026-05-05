9d3f6b9ad85a8eca62743691e68e1281a8e11793
sonal kumar shrivastawa <sonalkumar200429@gmail.com>
2026-05-06 00:30:04 +0530

    Change server URL to local development address

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom'
 import {Provider} from "react-redux"
 import {store } from './redux/store.js'
 
-export const serverUrl ="https://real-time-chat-backend-yo31.onrender.com"
+export const serverUrl ="http://localhost:8000"
 createRoot(document.getElementById('root')).render(
  <BrowserRouter> 
  <Provider store={store} > 
