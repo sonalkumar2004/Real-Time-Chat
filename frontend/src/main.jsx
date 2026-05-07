4460a5c1b7beded7989e2670aa95c1b3e5d5f8c6
Sonal kumar Shrivastawa <sonalkumar200429@gmail.com>
2026-05-08 00:00:21 +0530

    Update server URL for backend connection

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom'
 import {Provider} from "react-redux"
 import {store } from './redux/store.js'
 
-export const serverUrl ="https://real-time-chat-backend-yo31.onrender.com"
+export const serverUrl ="https://real-time-chatbackend.onrender.com"
 createRoot(document.getElementById('root')).render(
  <BrowserRouter> 
  <Provider store={store} > 
